15abdb5d106c96900c726db255443d39b1806e9d
jokyo02 <149650929+jokyo02@users.noreply.github.com>
2024-10-13 20:44:50 +0800

    Update main.go

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 	http.HandleFunc("/th/", helper.Middleware(api.Th))
 	http.HandleFunc("/designer/", helper.Middleware(api.Designer))
 
-	http.HandleFunc("/rp", helper.Middleware(api.Rpcore))
+	http.HandleFunc("/rp/", helper.Middleware(api.Rpcore))
 
 	http.HandleFunc("/edgesvc/", helper.Middleware(api.Edgesvc))
 	http.HandleFunc("/sydney/", helper.Middleware(wss_api.Sydney))
